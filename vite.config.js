e70db93b3818794402ac6e012c31e458801288b7
Bexi Castillo <bixi.castillo@Mac.lan>
2026-04-18 21:46:48 -0500

    Fix: Set static filenames for Squarespace

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // This keeps the filenames consistent so your Squarespace links never break
+        entryFileNames: `assets/scs-bundle.js`,
+        chunkFileNames: `assets/scs-bundle.js`,
+        assetFileNames: `assets/scs-bundle.[ext]`
+      }
+    }
+  }
 })
