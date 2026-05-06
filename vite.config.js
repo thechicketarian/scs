733769613273d7171ebf1b9db3158169bcebc40b
Bexi Castillo <bixi.castillo@Mac.lan>
2026-05-06 01:10:02 -0500

    Updating

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,19 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         // This keeps the filenames consistent so your Squarespace links never break
-//         entryFileNames: `assets/scs-bundle.js`,
-//         chunkFileNames: `assets/scs-bundle.js`,
-//         assetFileNames: `assets/scs-bundle.[ext]`
-//       }
-//     }
-//   }
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        format: 'iife', // This is the magic fix for injection
-        name: 'SCS_App',
+        // This keeps the filenames consistent so your Squarespace links never break
         entryFileNames: `assets/scs-bundle.js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/scs-bundle.[ext]`,
+        chunkFileNames: `assets/scs-bundle.js`,
+        assetFileNames: `assets/scs-bundle.[ext]`
       }
     }
   }
 })
+
+
