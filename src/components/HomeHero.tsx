08ecc362cf5148281f64875750aa0ce729685905
Bexi Castillo <bixi.castillo@SKCBexiC954.local>
2026-06-09 12:52:05 -0500

    updating homehero snipped

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -10,21 +10,21 @@ export default function HomeHero() {
         <div className="scs-home-hero-panel-1-wrapper-2">
             <div className="scs-home-hero-div1">
 
-              
+
 
                 <div className="scs-panel-1-inner-2" >
-                                  {/* <div className="panel-1-bar-2">
-                                        <div className="scs-text-span-sm">
-                soccer
-               </div>
-                                         <div className="panel-skc-logo">
-                <img src="https://scs-ochre.vercel.app/icons/SKC-Wordmark_SportingKansasCity-LightBG.svg" alt="Sporting Kansas City Logo"/>
-               </div>
-               
-               <div className="scs-text-span-sm">
-                summer
-               </div>
-        </div> */}
+                    {/* <div className="panel-1-bar-2">
+                        <div className="scs-text-span-sm">
+                            soccer
+                        </div>
+                        <div className="panel-skc-logo">
+                            <img src="https://scs-ochre.vercel.app/icons/SKC-Wordmark_SportingKansasCity-LightBG.svg" alt="Sporting Kansas City Logo" />
+                        </div>
+
+                        <div className="scs-text-span-sm">
+                            summer
+                        </div>
+                    </div> */}
                     <h1> The world's game is here</h1>
                     {/* <h1>
                     <span className="text-indigo">Our Game.</span>
@@ -39,21 +39,24 @@ export default function HomeHero() {
 
                           <FullSchedule mode="upcoming"/>
                     </div> */}
-                    {/* <div>
+            {/* <div>
                      <img src="https://scs-ochre.vercel.app/icons/26-SCS-SoccerBall.svg" width={200}/>
                     </div> */}
-                     <div className="parent">
-      <button className="div1">tickets<span className="material-symbols-outlined">
-arrow_right_alt
-</span></button>
-        <button className="div2">experiences<span className="material-symbols-outlined">
-arrow_right_alt
-</span></button>
-        <button className="div3">concerts<span className="material-symbols-outlined">
-arrow_right_alt
-</span></button>
-    </div>
-  
+            <div className="parent">
+                <a className="div1" href="https://www.soccercapitalsummer.com/tickets">tickets<span className="material-symbols-outlined">
+                    arrow_right_alt
+                </span></a>
+                <a className="div2" href="https://www.soccercapitalsummer.com/experience">experiences<span className="material-symbols-outlined">
+                    arrow_right_alt
+                </span></a>
+                <a className="div3" href="https://www.soccercapitalsummer.com/concerts">concerts<span className="material-symbols-outlined">
+                    arrow_right_alt
+                </span></a>
+                <a className="div4" href="https://www.soccercapitalsummer.com/schedule">schedule<span className="material-symbols-outlined">
+                    arrow_right_alt
+                </span></a>
+            </div>
+
         </div>
     );
 }
